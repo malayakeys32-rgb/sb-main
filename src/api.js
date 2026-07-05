41ff54a7b1437f79fd7a20726ee7b57024043980
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-05 04:52:46 -0700

    Reorder apiGet and apiPost functions

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
 const API_BASE = "https://sb-main-back.onrender.com";
 
-export async function apiGet(path) {
-  const res = await fetch(`${API_BASE}${path}`);
-  return res.json();
-}
-
 export async function apiPost(path, data) {
   const res = await fetch(`${API_BASE}${path}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
   });
+  return res.json();
+}
+
+export async function apiGet(path) {
+  const res = await fetch(`${API_BASE}${path}`);
+  return res.json();
+}
