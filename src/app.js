839159f5b5406e550ea2ccb2b8790bd377c929eb
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-06 13:36:31 -0700

    Reorder import statements in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
