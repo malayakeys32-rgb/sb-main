2947bdc91f32e0b456c15bec9d63637722fdb789
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 18:54:04 -0700

    Add organization routes with protection middleware

--- backend/routes/orgRoutes.js
+++ backend/routes/orgRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { createOrg, getOrgs } from "../controllers/orgController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/create", protect, createOrg);
+router.get("/all", protect, getOrgs);
+
+export default router;
 
