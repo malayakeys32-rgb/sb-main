a45d88fc086fb958ee9d257884b7f0ee5a17a00d
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 19:11:53 -0700

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { getUsers, getLogs } from "../controllers/adminController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/users", protect, getUsers);
+router.get("/logs", protect, getLogs);
+
+export default router;
 
