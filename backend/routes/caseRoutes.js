ab74f860eef2272d007522b7365cd6d3bbcf775f
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 18:53:41 -0700

    Add case routes for creating and retrieving cases

--- backend/routes/caseRoutes.js
+++ backend/routes/caseRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { createCase, getCases } from "../controllers/caseController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/create", protect, createCase);
+router.get("/all", protect, getCases);
+
+export default router;
 
