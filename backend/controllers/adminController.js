8d0cd34248bef3af7837171e9e0f621243c48128
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 18:52:17 -0700

    Add user and audit log retrieval functions

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,1 +1,21 @@
+import { User } from "../models/User.js";
+import { AuditLog } from "../models/AuditLog.js";
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json({ success: true, users });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getLogs = async (req, res, next) => {
+  try {
+    const logs = await AuditLog.find().sort({ createdAt: -1 });
+    res.json({ success: true, logs });
+  } catch (err) {
+    next(err);
+  }
+};
 
