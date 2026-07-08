fd7bc6789625356b539c8059893c471d17fa2e92
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 18:51:59 -0700

    Add createOrg and getOrgs functions

--- backend/controllers/orgController.js
+++ backend/controllers/orgController.js
@@ -1,1 +1,21 @@
+import { Org } from "../models/Org.js";
+
+export const createOrg = async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const org = await Org.create({ name, description, owner: req.user._id });
+    res.json({ success: true, org });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOrgs = async (req, res, next) => {
+  try {
+    const orgs = await Org.find({ owner: req.user._id });
+    res.json({ success: true, orgs });
+  } catch (err) {
+    next(err);
+  }
+};
 
