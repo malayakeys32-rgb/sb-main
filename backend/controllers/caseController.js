7297836f8a8f2e6deb9747c12a9e47220d5ee577
malayakeys32-rgb <malayakeys32@gmail.com>
2026-07-07 18:51:28 -0700

    Add createCase and getCases functions

--- backend/controllers/caseController.js
+++ backend/controllers/caseController.js
@@ -1,1 +1,25 @@
+import { Case } from "../models/Case.js";
+
+export const createCase = async (req, res, next) => {
+  try {
+    const { title, notes } = req.body;
+    const newCase = await Case.create({
+      title,
+      notes,
+      owner: req.user._id
+    });
+    res.json({ success: true, case: newCase });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCases = async (req, res, next) => {
+  try {
+    const cases = await Case.find({ owner: req.user._id });
+    res.json({ success: true, cases });
+  } catch (err) {
+    next(err);
+  }
+};
 
